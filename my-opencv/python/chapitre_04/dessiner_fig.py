502829bdf50a643e2b27d1eae6f5c232f32a7af7
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-07 17:22:56 +0100

    dessiner une ligne

---
+++ my-opencv/python/chapitre_04/dessiner_fig.py
@@ -0,0 +1,16 @@
+import cv2
+
+import numpy as np
+
+image = np.zeros((512, 512, 3), np.uint8)
+
+image[100:200, 50:400] = 25, 0, 180
+
+cv2.line(image, (0, 0), (512, 512), (0, 252, 0), 3)
+cv2.line(image, (0, 512), (512, 0), (0, 252, 0), 3)
+
+cv2.line(image, (0, 256), (512, 256), (512, 252, 0), 3)
+
+cv2.imshow('dessiner une fig', image)
+
+cv2.waitKey(0)
