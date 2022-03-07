c35a869ddf77c4c7e35d6043213059cd486e9c90
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-07 16:57:06 +0100

    comment redimensionner une image

--- my-opencv/python/chapitre_03/how_to_resiz_image.py
+++ my-opencv/python/chapitre_03/how_to_resiz_image.py
@@ -8,8 +8,13 @@ image = cv2.imread(image_ul)
 # Voir les dimensions de mon image
 print(image.shape)
 
+# comment redimensionner une image
+imageRedimensionne = cv2.resize(image, (400, 300))
+print(imageRedimensionne.shape)
+
 # Afficher image
 cv2.imshow('ma premiere image', image)
+cv2.imshow('image redimensionner', imageRedimensionne)
 
 # Le temps d'affichage de l'image 0: infini et n: millisecond
-cv2.waitKey(2000)
+cv2.waitKey(0)
