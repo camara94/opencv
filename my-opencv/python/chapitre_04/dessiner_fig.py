4aa87c9f7a37cac13c46fb22ba2a4503271646ca
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-07 17:39:59 +0100

    dessine cercle

--- my-opencv/python/chapitre_04/dessiner_fig.py
+++ my-opencv/python/chapitre_04/dessiner_fig.py
@@ -11,7 +11,9 @@ cv2.line(image, (0, 512), (512, 0), (0, 252, 0), 3)
 
 cv2.line(image, (0, 256), (512, 256), (512, 252, 0), 3)
 
-cv2.rectangle(image, (0, 0), (512, 256), (512, 252, 0), 3)
+cv2.rectangle(image, (0, 0), (512, 256), (512, 252, 0), 4)
+
+cv2.circle(image, (256, 256), 45, (0, 0, 45), 4)
 
 cv2.imshow('dessiner une fig', image)
 
