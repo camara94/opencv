ba6c74716108be5233914f107738e43deae0f2c6
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-07 18:11:24 +0100

    perspective

--- my-opencv/python/chapitre_05/perspective.py
+++ my-opencv/python/chapitre_05/perspective.py
@@ -1,11 +1,22 @@
 import cv2
 import numpy as np
 
+width, heigth = 350, 400
+
+pts1 = np.float32([[646, 44], [1186, 44], [640, 387], [1185, 388]])
+pts2 = np.float32([[0, 0], [width, 0], [0, heigth], [width, heigth]])
+
+matrice = cv2.getPerspectiveTransform(pts1, pts2)
+
+
 image_url = '../../../images/card.png'
 
 image = cv2.imread(image_url)
+
+image2 = cv2.warpPerspective(image, matrice, (width, heigth))
 print(image.shape)
 
 cv2.imshow('card', image)
+cv2.imshow('card 2', image2)
 
 cv2.waitKey(0)
