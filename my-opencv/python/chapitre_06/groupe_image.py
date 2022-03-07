7e8694b1830755e6f327204363666a22fcc9d5f6
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-07 18:26:03 +0100

    grouper les images

--- my-opencv/python/chapitre_06/groupe_image.py
+++ my-opencv/python/chapitre_06/groupe_image.py
@@ -1,4 +1,14 @@
 import cv2
 import numpy as np
 
+image_url = '../../../images/photo.jpg'
 
+image = cv2.imread(image_url)
+#image = image.resize(image, (400, 300))
+
+imageVerticales = np.vstack((image, image))
+imageHorizontales = np.hstack((image, image))
+
+cv2.imshow('grouper image v', imageVerticales)
+cv2.imshow('grouper image h', imageHorizontales)
+cv2.waitKey(0)
