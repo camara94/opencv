0a1dbd5c08625c5a75586b43a4136e9ebe169f3c
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-08 15:08:05 +0100

    hsv

--- my-opencv/python/chapitre_07/detecte_objet.py
+++ my-opencv/python/chapitre_07/detecte_objet.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def empty():
+def empty(a):
     pass
 
 
@@ -11,12 +11,24 @@ image_url = '../../../images/photo.jpg'
 cv2.namedWindow('Trackbars')
 cv2.resizeWindow('Trackbars', 640, 340)
 cv2.createTrackbar('Hue Min', 'Trackbars', 0, 179, empty)
+cv2.createTrackbar('Hue Max', 'Trackbars', 179, 179, empty)
+cv2.createTrackbar('Sat Min', 'Trackbars', 0, 255, empty)
+cv2.createTrackbar('Sat Max', 'Trackbars', 255, 255, empty)
+cv2.createTrackbar('Val Min', 'Trackbars', 0, 255, empty)
+cv2.createTrackbar('Val Max', 'Trackbars', 255, 255, empty)
 
-image = cv2.imread(image_url)
+while True:
+    image = cv2.imread(image_url)
+    imagesHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos('Hue Min', 'Trackbars')
+    h_max = cv2.getTrackbarPos('Hue Max', 'Trackbars')
+    s_min = cv2.getTrackbarPos('Sat Min', 'Trackbars')
+    s_max = cv2.getTrackbarPos('Sat Max', 'Trackbars')
+    v_min = cv2.getTrackbarPos('Val Min', 'Trackbars')
+    v_min = cv2.getTrackbarPos('Val Max', 'Trackbars')
+    print(h_min)
 
-imagesHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    cv2.imshow('image reel', image)
+    cv2.imshow('image hsv', imagesHSV)
 
-cv2.imshow('image reel', image)
-cv2.imshow('image hsv', imagesHSV)
-
-cv2.waitKey(0)
+    cv2.waitKey(1)
